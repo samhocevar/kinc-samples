f8a0ff44daa320acafab109ff957204f526260fd
Sam Hocevar <sam@hocevar.net>
2022-10-11 23:45:06 +0200

    Fix OpenGL rendering

--- two-textures/shaders/shader.frag.glsl
+++ two-textures/shaders/shader.frag.glsl
@@ -1,13 +1,13 @@
 #version 450
 
 out vec4 FragColor;
-in vec2 uv;
+in vec2 pass_uv;
 
 uniform sampler2D image;
 uniform sampler2D palette;
 
 void main()
 {
-    float shade = texture(image, uv).x;
+    float shade = texture(image, pass_uv).x;
     FragColor = texture(palette, vec2(shade, 0.5));
 }
--- two-textures/shaders/shader.vert.glsl
+++ two-textures/shaders/shader.vert.glsl
@@ -3,10 +3,10 @@
 in vec2 pos;
 in vec2 uv;
 
-out vec2 ouv;
+out vec2 pass_uv;
 
 void main()
 {
-    ouv = uv;
+    pass_uv = uv;
     gl_Position = vec4(pos.x, pos.y, 0.5, 1.0);
 }
